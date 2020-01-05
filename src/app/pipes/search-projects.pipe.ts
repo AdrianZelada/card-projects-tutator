01443380d054594cb8cf176a57470154271900bf
adrian zelada torrez <zelada_torrez@hotmail.com>
2020-01-05 02:04:18 -0400

    delete console

--- src/app/pipes/search-projects.pipe.ts
+++ src/app/pipes/search-projects.pipe.ts
@@ -7,8 +7,6 @@ import {Project} from '../interfaces/project';
 export class SearchProjectsPipe implements PipeTransform {
 
   transform(value: Array<Project>, args?: any): any {
-    console.log(value)
-      console.log(args);
       return value.filter((project: Project) => {
         return project.title.toLowerCase().search(args) !== -1;
       });
